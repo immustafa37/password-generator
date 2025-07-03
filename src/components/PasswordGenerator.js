6b1c1131b07662293f248f98de5b7ee0d0fb3f33
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-02 20:14:26 -0700

    add component passwordgenerator

---
+++ src/components/PasswordGenerator.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+const PasswordGenerator = () => {
+  const [length, setLength] = useState(8);
+  const [includeUppercase, setIncludeUppercase] = useState(true);
+  const [includeNumbers, setIncludeNumbers] = useState(true);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const generatePassword = () => {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+-={}[]:;<>,.?/";
+
+    let characters = lowercaseChars;
+    if (includeUppercase) characters += uppercaseChars;
+    if (includeNumbers) characters += numberChars;
+    if (includeSymbols) characters += symbolChars;
+
+    let generated = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      generated += characters[randomIndex];
+    }
+
+    setPassword(generated);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(password);
+    alert("Password copied to clipboard!");
+  };
+
+  return (
+    <div className="generator">
+      <div className="output">
+        <input type="text" value={password} readOnly />
+        <button onClick={copyToClipboard}>Copy</button>
+      </div>
+
+      <div className="controls">
+        <label>
+          Length: {length}
+          <input
+            type="range"
+            min="4"
+            max="32"
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={includeUppercase}
+            onChange={() => setIncludeUppercase(!includeUppercase)}
+          />
+          Include Uppercase
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={includeNumbers}
+            onChange={() => setIncludeNumbers(!includeNumbers)}
+          />
+          Include Numbers
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={includeSymbols}
+            onChange={() => setIncludeSymbols(!includeSymbols)}
+          />
+          Include Symbols
+        </label>
+      </div>
+
+      <button className="generate-btn" onClick={generatePassword}>
+        Generate Password
+      </button>
+    </div>
+  );
+};
+
+export default PasswordGenerator;
